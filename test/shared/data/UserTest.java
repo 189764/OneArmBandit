6ab9326c41a67f0949a163222f5c65ac0840a7a7
Karolina Kmiecik <Karolina Kmiecik>
2016-09-06 21:55:30 +0200

    UserTest

--- test/shared/data/UserTest.java
+++ test/shared/data/UserTest.java
@@ -15,5 +15,23 @@ public class UserTest {
 		int u = 10;
 		when(user.getStake()).thenReturn(u);
 	}
+	@Test
+	public void getLoginTest() {
+		User user = mock(User.class);
+		String u = "ania";
+		when(user.getLogin()).thenReturn(u);
+	}
+	@Test
+	public void getPasswordTest() {
+		User user = mock(User.class);
+		String u = "haslo";
+		when(user.getPassword()).thenReturn(u);
+	}
+	@Test
+	public void getPointsTest() {
+		User user = mock(User.class);
+		int u = 100;
+		when(user.getPoints()).thenReturn(u);
+	}
 
 }
