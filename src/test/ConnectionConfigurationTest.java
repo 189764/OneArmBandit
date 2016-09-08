b0381a687687c8263f59bbb4450c44284587fccc
sebastiann.swiatek@gmail.com <Sebastiann.Swiatek@gmail.com>
2016-09-08 03:08:43 +0200

    Added conn conf test class

--- src/test/ConnectionConfigurationTest.java
+++ src/test/ConnectionConfigurationTest.java
@@ -1,16 +1,33 @@
 package test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import shared.ConnectionConfiguration;
+
+@RunWith(MockitoJUnitRunner.class)
 
 public class ConnectionConfigurationTest {
 
-
+	
+	ConnectionConfiguration conf;
+	
+	@Before
+	public void setUp() {
+		
+	conf = ConnectionConfiguration.buildConfiguration();
+		
+	}
+	
 
 }
